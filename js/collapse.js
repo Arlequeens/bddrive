edc9c03df4c74c8a104516c98cd63db8095beaa0
arlequeens <1tarnaudin@gmail.com>
2020-12-30 21:54:47 +0100

    collapse v1

--- js/collapse.js
+++ js/collapse.js
@@ -1,3 +1,13 @@
 window.onresize = function(event) {
     $('#collapsePanier').collapse('show');
 };
+
+$('#collapsePanier').on('hide.bs.collapse', function () {
+    $('.fa-angle-up').addClass('d-none');
+    $('.fa-angle-down').removeClass('d-none');
+})
+
+$('#collapsePanier').on('show.bs.collapse', function () {
+    $('.fa-angle-down').addClass('d-none');
+    $('.fa-angle-up').removeClass('d-none');
+})
