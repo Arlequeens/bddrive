61d234b55ba9daf282d548af910f2b8e403a2329
rxaard <55988013+rxaard@users.noreply.github.com>
2021-01-05 08:50:40 +0100

    dynamique js

---
+++ js/navigation.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    $("#navSelections").click(function () {
+        document.location.href = "selection.html";
+    });
+
+    $("#navCatalogue").click(function () {
+        document.location.href = "catalogue.html";
+    });
+
+    $("#navMonPanier").click(function () {
+        document.location.href = "panier.html";
+    });
+
+    $("#navSeConnecter").click(function () {
+        document.location.href = "connexion.html";
+    });
+
+    $(".navbar-brand").click(function () {
+        document.location.href = "home.html";
+    });
+
+    $(".detail").click(function () {
+        document.location.href = "panier.html";
+    });
+
+    $("#ParcourirCtlg").click(function () {
+        document.location.href = "catalogue.html";
+    });
+
+});
+
+
+function goBack() {
+    window.history.back();
+}
+
+
