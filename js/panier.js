aca77e8d52d1a2fc88b606a7654ae10e49c35fe9
arlequeens <1tarnaudin@gmail.com>
2021-01-08 20:07:46 +0100

    update

--- js/panier.js
+++ js/panier.js
@@ -35,44 +35,6 @@ $(document).ready(function () {
 
 });
 
-function cloneArticlePanier (album, nbreArticle) {
-    var titre = album.titre;
-    var auteur = auteurs.get(album.idAuteur).nom;
-    var prix = (parseFloat(album.prix) * nbreArticle).toFixed(2);
-    var srcImage = sourceImage(album);
-
-    return   '<div class="card mb-3">'
-            +'   <div class="row no-gutters">'
-            +'        <div class="col-4">'
-            +'            <img src="'+ srcImage +'" class="card-img rounded" alt="">'
-            +'        </div>'
-            +'        <div class="col-8">'
-            +'            <div class="card-body d-flex flex-column justify-content-between align-items-start h-100">'
-            +'                <div>'
-            +'                    <h1 class="card-title h5">'+ titre +'</h2>'
-            +'                    <h2 class="card-subtitle mb-2 text-muted h6">'+ auteur +'</h2>'
-            +'                </div>'
-            +'                <div class="row d-flex align-items-center">'
-            +'                    <p class="col-5 mb-0">'+ prix +' €</p>'
-            +'                    <div class="col-5 pl-0">'
-            +'                        <div class="input-group input-group-sm">'
-            +'                            <div class="input-group-prepend cursor-pointer moins-article">'
-            +'                                <span class="input-group-text">-</span>'
-            +'                            </div>'
-            +'                            <input type="text" class="form-control text-center bg-transparent" disabled placeholder="'+ nbreArticle +'">'
-            +'                            <div class="input-group-append cursor-pointer plus-article">'
-            +'                               <span class="input-group-text">+</span>'
-            +'                            </div>'
-            +'                       </div>'
-            +'                   </div>'
-            +'                   <i class="col-2 fa fa-trash fa-lg cursor-pointer suppr-article"></i>'
-            +'               </div>'
-            +'           </div>'
-            +'       </div>'
-            +'   </div>'
-            +'</div>';
-}
-
 // Afficher et abonner les évenement au panier et panier simplifié
 function majPanier () {
 
@@ -184,4 +146,182 @@ function majPanier () {
         $('#liste-BD-panier').append('<p>Votre panier est vide</p>');
         $('.liste-BD-panier-simplifie').append('<p class="mb-3 ml-3">Votre panier est vide</p>');
     }
+}
+
+// Transforme le panier du local storage en tableau
+function toTabPanierLocal(panierLocal) {
+
+    var tabPanier = [];
+    var pairs = panierLocal.split(",");
+    for (i in pairs) {
+        if(pairs[i] !== "") {
+            tabPanier.push(pairs[i].split(":"));
+        }
+    }
+    return tabPanier;
+}
+
+// Ecrit la valeur du localStorage "panierBD" à partir du tableau
+function majPanierLocal(tabPanier) {
+    panierLocal = genereValeurPanierLocal(tabPanier);
+    localStorage.setItem('panierBD',panierLocal);
+}
+
+// Génère la valeur du localStorage "panierBD" à partir du tableau
+function genereValeurPanierLocal(tabPanier) {
+    var pairs = [];
+    if (tabPanier.lenght !== 0) {
+        for (i in tabPanier) {
+            pairs.push(tabPanier[i].join(':'));
+        }
+    }
+    return pairs.join(',');
+}
+
+// Ajout d'un album au panier dans le tableau
+function ajoutPanier(idAlbum, tabPanier) {
+    
+    var alreadyExist = false;
+    for (i in tabPanier) {
+
+        // Incrémente le nombre d'article si l'article existe déjà
+        if(tabPanier[i][0] === idAlbum) {
+            var nbreArticle = parseInt(tabPanier[i][1]);
+            nbreArticle += 1;
+            tabPanier[i][1] = nbreArticle.toString();
+            alreadyExist = true;
+        }
+    }
+    if(!alreadyExist) {
+        var pair = [idAlbum, "1"];
+        tabPanier.push(pair);
+    }
+}
+
+// Obtient le nombre d'article pour un article du panier localStorage
+function getArticleNbrePanier(idArticle) {
+    var panierLocal = localStorage.getItem('panierBD');
+    var tabPanier = toTabPanierLocal(panierLocal);
+    for (i in tabPanier) {
+        if (tabPanier[i][0] === idArticle.toString()) {
+            return tabPanier[i][1];
+        }
+    }
+}
+
+// Attribut le nombre d'article pour un article dans le panier localStorage
+function setArticleNbrePanier(idArticle, nbArticle) {
+    var panierLocal = localStorage.getItem('panierBD');
+    var tabPanier = toTabPanierLocal(panierLocal);
+    for (i in tabPanier) {
+        if (tabPanier[i][0] === idArticle.toString()) {
+            tabPanier[i][1] = nbArticle;
+            break;
+        }
+    }
+    majPanierLocal(tabPanier);
+}
+
+// Supprime un article du panier localStorage
+function deleteArticlePanier(idArticle) {
+    var newTabPanier = [];
+    var panierLocal = localStorage.getItem('panierBD');
+    var tabPanier = toTabPanierLocal(panierLocal);
+    for (i in tabPanier) {
+        if (tabPanier[i][0] !== idArticle.toString()) {
+            newTabPanier.push(tabPanier[i]);
+        }
+    }
+    majPanierLocal(newTabPanier);
+}
+
+// Calcul le total du panier à partir du panier localStorage
+function calculTotalPanier() {
+    var totalPanier = 0;
+    var panierLocal = localStorage.getItem('panierBD');
+    var tabPanier = toTabPanierLocal(panierLocal);
+    for (i in tabPanier) {
+        totalPanier += calculTotalPrixArticle(tabPanier[i][0], parseInt(tabPanier[i][1]));
+    }
+    return totalPanier;
+}
+
+// Calcul le total du prix d'un article avec son nombre
+function calculTotalPrixArticle(idAlbum, nbArticle) {
+    return parseFloat(albums.get(idAlbum).prix) * nbArticle;
+}
+
+// Calcul le nombre total d'articles à partir du panier localStorage
+function calculNbreArticleTotal() {
+    var nbreArticleTotal = 0;
+    var panierLocal = localStorage.getItem('panierBD');
+    var tabPanier = toTabPanierLocal(panierLocal);
+    for (i in tabPanier) {
+        nbreArticleTotal += parseInt(tabPanier[i][1]);
+    }
+    return nbreArticleTotal;
+}
+
+// Génération de la div pour une BD du panier détaillé
+// IN: album, nbre d'article
+function cloneArticlePanier (album, nbreArticle) {
+    var titre = album.titre;
+    var auteur = auteurs.get(album.idAuteur).nom;
+    var prix = (parseFloat(album.prix) * nbreArticle).toFixed(2);
+    var srcImage = sourceImage(album);
+
+    return   '<div class="card mb-3">'
+            +'   <div class="row no-gutters">'
+            +'        <div class="col-4">'
+            +'            <img src="'+ srcImage +'" class="card-img rounded" alt="">'
+            +'        </div>'
+            +'        <div class="col-8">'
+            +'            <div class="card-body d-flex flex-column justify-content-between align-items-start h-100">'
+            +'                <div>'
+            +'                    <h1 class="card-title h5">'+ titre +'</h2>'
+            +'                    <h2 class="card-subtitle mb-2 text-muted h6">'+ auteur +'</h2>'
+            +'                </div>'
+            +'                <div class="row d-flex align-items-center">'
+            +'                    <p class="col-5 mb-0">'+ prix +' €</p>'
+            +'                    <div class="col-5 pl-0">'
+            +'                        <div class="input-group input-group-sm">'
+            +'                            <div class="input-group-prepend cursor-pointer moins-article">'
+            +'                                <span class="input-group-text">-</span>'
+            +'                            </div>'
+            +'                            <input type="text" class="form-control text-center bg-transparent" disabled placeholder="'+ nbreArticle +'">'
+            +'                            <div class="input-group-append cursor-pointer plus-article">'
+            +'                               <span class="input-group-text">+</span>'
+            +'                            </div>'
+            +'                       </div>'
+            +'                   </div>'
+            +'                   <i class="col-2 fa fa-trash fa-lg cursor-pointer suppr-article"></i>'
+            +'               </div>'
+            +'           </div>'
+            +'       </div>'
+            +'   </div>'
+            +'</div>';
+}
+
+// Génération de la div pour une BD du panier simplifié
+// IN: album
+function cloneAlbumPanierSimplifie (album) {
+    var titre = album.titre;
+    var auteur = auteurs.get(album.idAuteur).nom;
+    var prix = album.prix + "€";
+    var srcImage = sourceImageMini(album);
+
+    return   '<div class="card col-3 col-lg-12 border-0 mb-3 px-1">'
+            +'    <div class="row no-gutters">'
+            +'        <div class="col-lg-6">'
+            +'            <img src="'+ srcImage +'" class="card-img rounded" alt="">'
+            +'        </div>'
+            +'        <div class="col-lg-6">'
+            +'            <div class="card-body d-none d-lg-block">'
+            +'                <h1 class="card-title h5">'+ titre +'</h2>'
+            // +'                <h2 class="card-subtitle mb-2 text-muted h6">'+ auteur +'</h2>'
+            // +'                <p class="card-text">'+ prix +'</p>'
+            +'            </div>'
+            +'        </div>'
+            +'    </div>'
+            +'</div>';
 }
